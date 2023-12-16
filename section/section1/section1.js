8e4356719c85a1f5af55ed9d64e62335b1d12d27
mion / Mion Higaki <m.higaki@4nonome.com>
2023-12-16 10:45:46 +0900

    Update section1.js

--- section/section1/section1.js
+++ section/section1/section1.js
@@ -2,18 +2,12 @@ q1 = "//";
 
 q2 = "var";
 
-q3 = `let country="Japan";
-      country="America"`;
+q3 = 'let country = "Japan"country = "America"';
 
-q4 = `const japan={
-      id=1,
-      capital ="東京"
-      };
-      japan.key=long;
-      japan.language="日本語";`;
+q4 ='const japan = {id: 1,capital: "東京"}japan.lang = "日本語"';
 
-q5 = [1,2,5,6];
+q5 = [2,5,6];
 
-q6 = "パスカルケース";
+q6 = "キャラメルケース";
 
 module.exports = { q1, q2, q3, q4, q5, q6 };
